670f9754498d5b63c75deeae2c5347d1160a640a
unknown <davisish@yahoo.com>
2016-03-17 00:48:49 -0300

    Initial commit

---
+++ app/src/androidTest/java/cs1635/triviup/ApplicationTest.java
@@ -0,0 +1,13 @@
+package cs1635.triviup;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
